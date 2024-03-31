adda0e7b04a7d429b56abce0d8d28ef04b1bdcd2
amiablechief <amiablechief@outlook.com>
2024-03-31 14:47:33 -0400

    First set of modifications on qmk_userspace. Disable buzzer, use animations.

--- users/unicorne_km/rules.mk
+++ users/unicorne_km/rules.mk
@@ -1,1 +1,9 @@
 SRC += unicorne_km.c
+
+VIA_ENABLE = no
+
+# Disable the piezo buzzer on Unicorne
+AUDIO_ENABLE = no
+
+# Your mouse will drift without a joystick by default
+POINTING_DEVICE_ENABLE = no
--- users/unicorne_km/unicorne_km.h
+++ users/unicorne_km/unicorne_km.h
@@ -1,6 +1,13 @@
 #ifndef USERSPACE
 #define USERSPACE
 
+// The default max layer count is 4
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+// List of all possible animations:
+// jhttps://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgb_matrix.md#rgb-matrix-effects-idrgb-matrix-effects
+// TODO: Play around with RGB later
+
 #include "quantum.h"
 
 void my_custom_function(void);
